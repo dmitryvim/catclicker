310978f05ed8b13edb1dcde619a5669c0d2b40c5
dmitryvim <dmitry.mikhailovich@gmail.com>
2017-08-22 10:35:12 +0300

    add mvo catclicker

--- catclicker/js/catclicker.js
+++ catclicker/js/catclicker.js
@@ -1,35 +1,88 @@
+$(function(){
+    var model = {
+        clickers: [],
 
-function numberInput(count) {
-    
-    var main = document.getElementById("dmitry-parent")
-    main.innerHTML = "";
-    for(var i = 0; i < count; i++) {
-        createClicker(main, "clicker" + i);
+        init: function() {
+            this.clickers = [];
+        },
+
+        addClicker: function(name) {
+            this.clickers.push({
+                id: this.clickers.length,
+                name, 
+                src: "img/1784.png",
+                count: 0
+            });
+        },
+
+        click: function(id) {
+            this.clickers[id].count++;
+        }
     }
-}
 
-function createClicker(parent, id) {
-    var div = document.createElement("div");
+    var octupus = {
+        init: function() {
+            model.init();
+            view.init();
+        },
 
-    var img = document.createElement("img");
-    img.setAttribute("src", "img/1784.png");
-    img.onclick = function () {
-        clickMe(id);
+        addClicker: function(name) {
+            model.addClicker(name);
+            view.render();
+        },
+
+        clickClicker: function(id) {
+            model.click(id);
+            view.render();
+        },
+
+        clickers: function() {
+            return model.clickers;
+        }
     }
-    
 
-    var counter = document.createElement("div")
-    counter.id = id;
-    counter.innerText = "0";
+    var view = {
+        active_clicker: -1,
+        app_parent: document.getElementById("dmitry-parent"),
 
-    parent.appendChild(div);
-    div.appendChild(img);
-    div.appendChild(counter);
-    
-}
+        init: function() {
+            active_clicker: 0;
+        },
 
-function clickMe(counter_id) {
-    var el = document.getElementById(counter_id);
-    var count = parseInt(el.innerText) + 1;
-    el.innerHTML = count;
-};
+        renderObject: function(obj) {
+            var wrapper = document.createElement("div");
+            wrapper.appendChild(obj);
+            view.app_parent.appendChild(wrapper);
+        },
+
+        renderButton: function(clicker) {
+            var button = document.createElement("input");
+            button.setAttribute("type", "button");
+            button.value = clicker.name;
+            button.onclick = function () {
+                view.active_clicker = clicker.id;
+                octupus.clickClicker(clicker.id);
+            }
+            view.renderObject(button);
+        },
+
+        renderClicker: function(clicker) {
+            var span = document.createElement("span");
+            span.innerText = clicker.id + ": " + clicker.name + " - " + clicker.count;
+            view.renderObject(span);
+        },
+
+        render: function() {
+            this.app_parent.innerHTML = "";
+            var clickers = octupus.clickers();
+            clickers.forEach(view.renderButton);
+            if (this.active_clicker >= 0) {
+                view.renderClicker(clickers[this.active_clicker]);
+            }
+        }
+    }
+
+    octupus.addClicker("first");
+    octupus.addClicker("second");
+    octupus.addClicker("third");
+});
